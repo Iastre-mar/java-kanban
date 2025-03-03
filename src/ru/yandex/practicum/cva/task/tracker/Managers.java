7ff5f295bd629dc5f300bcb2212a86b4903cbdad
vchichvarin <vladimir150799@yandex.ru>
2025-03-03 19:52:57 +0300

    Managers don't operate with Singletons anymore

--- src/ru/yandex/practicum/cva/task/tracker/Managers.java
+++ src/ru/yandex/practicum/cva/task/tracker/Managers.java
@@ -1,23 +1,12 @@
 package ru.yandex.practicum.cva.task.tracker;
 
 public class Managers {
-    private static TaskManager defaultTaskManager;
-    private static HistoryManager defaultHistoryManager;
-
-    private Managers() {
-    }
 
     public static TaskManager getDefault() {
-        if (Managers.defaultTaskManager == null) {
-            Managers.defaultTaskManager = new InMemoryTaskManager();
-        }
-        return Managers.defaultTaskManager;
+        return new InMemoryTaskManager();
     }
 
     public static HistoryManager getDefaultHistory() {
-        if (Managers.defaultHistoryManager == null) {
-            Managers.defaultHistoryManager = new InMemoryHistoryManager();
-        }
-        return Managers.defaultHistoryManager;
+        return new InMemoryHistoryManager();
     }
 }
