60e64aecd8e6360a734995e2c139fc625845af4c
vchichvarin <vladimir150799@yandex.ru>
2025-03-24 19:52:33 +0300

    Simplified InMemoryHistoryManager

--- src/ru/yandex/practicum/cva/task/tracker/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/cva/task/tracker/InMemoryHistoryManager.java
@@ -1,172 +1,99 @@
 package ru.yandex.practicum.cva.task.tracker;
 
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private final MyLinkedHashSet<Task> tasks;
+    private final HashMap<Integer, Integer> map;
+    private final Node first = new Node();
+    private final Node last = new Node();
+    private int size;
 
 
     public InMemoryHistoryManager() {
-        this.tasks = new MyLinkedHashSet<>();
+        this.map = new HashMap<>();
+
+        first.next = last;
+        last.prev = first;
     }
 
     @Override
     public List<Task> getHistory() {
-        return tasks.getTasks();
+        return getTasks();
     }
 
     @Override
     public void add(Task task) {
         if (task != null) {
-            tasks.add(task.clone());
-        }
+            int taskId = task.getId();
 
+            if (map.containsKey(taskId)) {
+                int idxInList = map.get(taskId);
+                remove(idxInList);
+            }
+            map.put(taskId, size);
+            linkLast(task.clone());
+        }
     }
 
     @Override
     public void remove(int id) {
-        Task formalTask = new Task("Technical");
-        formalTask.setId(id);
-        tasks.remove(formalTask);
+
+        if (map.containsKey(id)) {
+            int idx = map.get(id);
+            map.remove(id);
+            Node node = find(idx);
+            removeNode(node);
+
+        }
     }
 
-    /**
-     * Нам неявно предлагают самим реализовать LinkedHashSet.
-     * <p>Можно ли оставить в таком виде чтобы не засорять код? </p>
-     */
-    private static class MyLinkedHashSet<T extends Task>
-            extends AbstractSet<T> {
-        private final HashMap<Integer, Integer> map;
-        private final MyLinkedList<T> list;
-
-
-        private MyLinkedHashSet() {
-            map = new HashMap<>();
-            list = new MyLinkedList<>();
-        }
-
-        public List<T> getTasks() {
-            return list.getTasks();
-        }
-
-        @Override
-        public Iterator<T> iterator() {
-            return list.iterator();
-        }
-
-        @Override
-        public int size() {
-            return list.size();
-        }
-
-        @Override
-        public boolean add(T t) {
-            int oldSize = size();
-            int taskId = t.getId();
-            if (map.containsKey(taskId)) {
-                int idxInList = map.get(taskId);
-                list.remove(idxInList);
-            }
-            map.put(taskId, size());
-            list.linkLast(t);
-
-            return size() > oldSize;
-        }
-
-        @Override
-        public boolean remove(Object o) {
-            int oldSize = size();
-            if (!(o instanceof Task task)) {
-                throw new IllegalArgumentException();
-            }
-
-            int taskId = task.getId();
-
-            if (map.containsKey(taskId)) {
-                int idx = map.get(taskId);
-                map.remove(taskId);
-                list.remove(idx);
-            }
-
-            return size() < oldSize;
-        }
-
-        private static class MyLinkedList<E> extends AbstractList<E> {
-            // Начало и конец являются лишь указателями и
-            //  не имеют собственного значения.
-            private final Node first = new Node();
-            private final Node last = new Node();
-            private int size;
-
-            private MyLinkedList() {
-                first.next = last;
-                last.prev = first;
-            }
-
-            public void linkLast(E value) {
-                Node newNode = new Node();
-                newNode.value = value;
-                Node prevLast = last.prev;
-                prevLast.next = newNode;
-                newNode.prev = prevLast;
-                newNode.next = last;
-                last.prev = newNode;
-                size++;
-            }
-
-            public E get(int index) {
-                Node resFind = find(index);
-                return (resFind == null) ? null : resFind.value;
-            }
-
-            // В случае поступления индекса меньше нуля
-            //  выкидывается NullPointerException (Так и предполагалась).
-            // Стоит ли добавлять секцию throws?
-
-            /**
-             * @throws NullPointerException in case of negative index
-             */
-            @Override
-            public E remove(int index) throws NullPointerException {
-                E res = null;
-                Node node = find(index);
-                if (node != null) {
-                    node.prev.next = node.next;
-                    node.next.prev = node.prev;
-                    res = node.value;
-                    size--;
-                }
-                return res;
-            }
-
-            public List<E> getTasks() {
-                return new ArrayList<>(this);
-            }
-
-            @Override
-            public int size() {
-                return this.size;
-            }
-
-            private Node find(int index) {
-                Node currNode = first;
-                while (index >= 0 && currNode != null) {
-                    currNode = currNode.next;
-                    index--;
-
-                }
-                return currNode;
-            }
-
-            private class Node {
-                private Node prev;
-                private E value;
-                private Node next;
-            }
-
+    private Node find(int index) {
+        Node currNode = first;
+        while (index >= 0 && currNode != null) {
+            currNode = currNode.next;
+            index--;
 
         }
+        return currNode;
+    }
+
+    private void linkLast(Task value) {
+        Node newNode = new Node();
+        newNode.value = value;
+        Node prevLast = last.prev;
+        prevLast.next = newNode;
+        newNode.prev = prevLast;
+        newNode.next = last;
+        last.prev = newNode;
+        size++;
+    }
+
+    private void removeNode(Node node) {
+        if (node != null) {
+            node.prev.next = node.next;
+            node.next.prev = node.prev;
+            size--;
+        }
+    }
+
+    private List<Task> getTasks() {
+        Node currNode = first;
+        List<Task> res = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            res.add(currNode.next.value);
+            currNode = currNode.next;
+        }
+        return res;
+    }
+
+    private class Node {
+        private Node prev;
+        private Task value;
+        private Node next;
+
     }
 
 
